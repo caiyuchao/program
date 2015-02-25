bb88dbc81a38b11a5eaee54779e776ef582c2505
yubo <yubo@xiaomi.com>
2015-02-25 22:21:45 +0800

    move cortex-tool

--- c/gdb/shared/run.sh
+++ c/gdb/shared/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cortex -i ./core -f reg,cal
+./cortex -i ./core -f reg,cal
 addr2line -e main 0x00000000ba45b67e 0x00000000004006a1
--- c/gdb/single/run.sh
+++ c/gdb/single/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cortex -i ./core -f reg,cal
+./cortex -i ./core -f reg,cal
 addr2line -e a.debug 0x0000000000400501 0x000000000040052c
