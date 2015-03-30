cf3bb90a43ef3d79e3da829d701a3e9c16aea61f
yubo <yubo@xiaomi.com>
2015-03-30 21:52:31 +0800

    add graph.go

--- go/02_graph.go
+++ go/02_graph.go
@@ -7,17 +7,17 @@ import (
 )
 
 type req struct {
-	cb   *chan string
+	cb   chan string
 	data string
 }
 
 var (
-	request_chan chan *req
+	request_chan chan req
 	update_chan  chan string
 )
 
-func foo(r *req) {
-	*r.cb <- strings.Join([]string{"hello", r.data}, ":")
+func foo(r req) {
+	r.cb <- strings.Join([]string{"ok", r.data}, ":")
 }
 
 func worker() {
@@ -25,7 +25,7 @@ func worker() {
 		select {
 		case msg1 := <-request_chan:
 			foo(msg1)
-			fmt.Println("work requst_chan received", msg1.data)
+			fmt.Println("work request_chan received", msg1.data)
 		case msg2 := <-update_chan:
 			fmt.Println("work update_chan received", msg2)
 		}
@@ -34,7 +34,7 @@ func worker() {
 
 func main() {
 
-	request_chan = make(chan *req)
+	request_chan = make(chan req)
 	update_chan = make(chan string)
 
 	go worker()
@@ -50,17 +50,17 @@ func main() {
 		}
 	}()
 
-	t2 := time.NewTicker(time.Millisecond * 500)
+	t2 := time.NewTicker(time.Millisecond * 1000)
 	go func() {
 		for range t2.C {
 			resp := make(chan string)
-			request := &req{
-				cb:   &resp,
+			request := req{
+				cb:   resp,
 				data: "hello",
 			}
-			fmt.Println("send request_chan", request,
-				"cb", request.cb,
-				"data", request.data)
+			//			fmt.Println("send request_chan", request,
+			//				"cb", request.cb,
+			//				"data", request.data)
 			request_chan <- request
 			r := <-resp
 			fmt.Println("received response", r)
