9f7aaa69ae2c43e48566cf44d147e1d3b0095fbe
yubo <yubo@xiaomi.com>
2015-05-15 11:05:35 +0800

    add debug.h for print log message

---
+++ c/log/b.c
@@ -0,0 +1,8 @@
+#define _DEBUG 7
+#include "debug.h"
+
+int main(){
+	dlog("hello world %d\n", 1);
+	elog("hello world %d\n", 1);
+	return 0;
+}
--- c/macro/str.c
+++ c/macro/str.c
@@ -1,5 +1,8 @@
 #define STR "heloo world"
 #define STR1 "#STR" 
+#ifndef STR2
+#error must have xxx
+#endif
 
 STR
 STR1
