d6333b28a5feb19b0aeb557dc5f4d15dbcb66a54
yubo <yubo@yubo.org>
2015-07-29 10:29:28 +0800

    update makefile

--- c/include/example.mk
+++ c/include/example.mk
@@ -4,27 +4,34 @@ proj_deps := $(foreach n,$(objs),$(if $($(n:.o=-objs)),$($(n:.o=-objs)),$(n)))
 
 proj_objs := $(subst .o,,$(objs))
 
-all: $(proj_deps) $(proj_objs) $(OBJS) $(EXAMPLES) 
+all: $(proj_objs) $(OBJS) $(EXAMPLES) 
 
 $(proj_objs): $(proj_deps)
 
-$(OBJS): $(addsuffix .o,$(OBJS))
+$(OBJS):%: %.o
+	$(CC) $< -o $@ $(LDFLAGS)
 
-%: 
+%.d: %.c
+	@set -e; rm -f $@; \
+	$(CC) -M $(CPPFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+
+$(proj_objs): %: 
 	$(CC) $(if $($(@)-objs), $($(@)-objs), $(@).o) -o $@ $(LDFLAGS)
 
-.o.c:
-	$(CC) $(CFLAGS) $< -c -o $@
+%.o: %.c
+	$(CC) -c $(CFLAGS) $< -o $@
 
 %.s: %.c
-	$(CC) $(CFLAGS) $< -S -o $@
+	$(CC) -S $(CFLAGS) $< -o $@
 
 %.e: %.c
-	$(CC) $(CFLAGS) $< -E -o $@
+	$(CC) -E $(CFLAGS) $< -o $@
 
 clean:
-	rm -rf $(EXAMPLES) $(OBJS) $(proj_objs) $(proj_deps)
+	rm -rf $(EXAMPLES) $(OBJS) $(proj_objs) $(proj_deps) $(addsuffix .o, $(OBJS))
 
 test:
-	echo $(proj_deps) $(proj_objs) $(OBJS) $(EXAMPLES) 
+	echo $(proj_deps) $(proj_objs) $(OBJS) $(EXAMPLES) $(addsuffix .o, $(OBJS)) 
 
