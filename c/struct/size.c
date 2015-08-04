7629b1fb03ae74905e4cd13947af38a25dd3dd0d
yubo <yubo@yubo.org>
2015-08-04 23:03:36 +0800

    add union&struct

--- c/struct/size.c
+++ c/struct/size.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+struct block_t {
+	char b[188];
+};
 
 struct foo_t{
 	uint32_t a;
 	uint32_t b;
-	char c[0];
+	union {
+		char d[0];
+		struct block_t e[0];
+	}c;
 };
 
 
 int main(){
+	struct foo_t *foo;
+	int i, j;
+
 	printf("struct foo_t size: %lu\n", sizeof(struct foo_t));
+	foo = (struct foo_t *)malloc(sizeof(struct foo_t) + 188*10);
+	for(i = 0; i<10; i++){
+		for(j=0; j<188; j++){
+			foo->c.e[i].b[j] = i+j;
+		}
+	}
+	for(i=0; i<1880; i++){
+		printf("%02x ", (uint8_t)foo->c.d[i]);
+		if(i%188 == 187) printf("\n");
+	}
 	return 0;
 }
