ec08027821790a36f15b84db7df014b67574082f
yubo <yubo@yubo.org>
2015-10-29 07:23:30 -0700

    add glog

---
+++ go/example/34_log.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+go run 34_log.go -log_dir="./34_log" -v=3
+go run 34_log.go -log_dir="./34_log" -v=3 -vmodule=34_log=4
+go run 34_log.go  -logtostderr=true -v=3
+go run 34_log.go  -logtostderr=true -v=3 -vmodule=34_log=4
+//	-logtostderr=false
+//		Logs are written to standard error instead of to files.
+//	-alsologtostderr=false
+//		Logs are written to standard error as well as to files.
+*/
+
+import (
+	"flag"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	flag.Parse()
+	glog.Info("hello")
+	glog.V(2).Infof("info %d", 2)
+	glog.V(3).Infof("info %d", 3)
+	glog.V(4).Infof("info %d", 4)
+	glog.Flush()
+}
