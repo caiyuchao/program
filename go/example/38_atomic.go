5a37545f51568097884efbfd9d714ed3dd338f22
yubo <yubo@yubo.org>
2015-12-10 18:41:34 +0800

    add golang atomic

---
+++ go/example/38_atomic.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"unsafe"
+)
+
+type t struct {
+	x int
+	y int
+}
+
+var p unsafe.Pointer
+
+func foo() *t {
+	return (*t)(atomic.LoadPointer(&p))
+}
+
+func bar() {
+	atomic.StorePointer(&p, unsafe.Pointer(&t{x: 2, y: 3}))
+}
+
+func main() {
+	p = unsafe.Pointer(&t{x: 1, y: 2})
+	fmt.Println(foo())
+	bar()
+	fmt.Println(foo())
+}
