0b5998f3ba702d9803e9052c54b70c49c81ae0cd
yubo <yubo@yubo.org>
2015-12-30 16:07:52 +0800

    update interface

--- go/gopl.io/07_interfaces.go
+++ go/gopl.io/07_interfaces.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bytes"
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"syscall"
 	"time"
 )
 
@@ -18,6 +20,8 @@ func section(s string) {
 	fmt.Printf("## %s\n", s)
 	log.SetPrefix(s + ": ")
 }
+
+type ByteCounter int
 
 func main() {
 	fmt.Println("# 7. Interfaces")
@@ -80,5 +84,89 @@ func main() {
 	}
 	section("7.8 The error Interface")
 	{
+		var err error = syscall.Errno(2)
+		fmt.Println(err.Error()) // "no such file or directory"
+		fmt.Println(err)         // "no such file or directory"
+	}
+	section("7.9 Example: Expression Evaluator")
+	{
+		//ch7/eval
+		//ch7/surface
+	}
+	section("7.10 Type Assertions")
+	{
+		var w io.Writer
+		w = os.Stdout
+		rw := w.(io.ReadWriter) // success: *os.File has both Read and Write
+		f := w.(*os.File)       // success: f == os.Stdout
+		//c := w.(*bytes.Buffer)  // panic: interface holds *os.File, not * bytes.Buffer
+
+		//w = new(ByteCounter)
+		//rw = w.(io.ReadWriter) // panic: *ByteCounter has no Read method
+
+		w = rw             // io.ReadWriter is assignable to io.Writer
+		w = rw.(io.Writer) // fails only if rw == nil
+		fmt.Printf("%#v, %#v\n", f, w)
+	}
+	{
+		var w io.Writer = os.Stdout
+		if f, ok := w.(*os.File); ok {
+			fmt.Println(f, ok)
+		} // success: ok, f == os.Stdout
+
+		if b, ok := w.(*bytes.Buffer); ok {
+			fmt.Println(b, ok)
+		} // failure: !ok, b == nil
+	}
+	section("7.11 Discriminating Errors with Type Assertions")
+	{
+		//os.PathError.Error()
+		_, err := os.Open("/no/such/file")
+		fmt.Println(err) // "open /no/such/file: No such file or directory"
+		fmt.Printf("%#v\n", err)
+		//Output:
+		//&os.PathError{Op:"open", Path:"/no/such/file", Err:0x2}
+	}
+	{
+		// IsNotExist returns a boolean indicating whether the error is known to
+		// report that a file or directory does not exist. It is satisfied by
+		// ErrNotExist as well as some syscall errors.
+		_, err := os.Open("/no/such/file")
+		fmt.Println(os.IsNotExist(err)) // "true"
+	}
+	section("7.12 Querying Behaviors with Interface Type Assertions")
+	{
+		//see net/http
+	}
+	section("7.13 Type Switches")
+	{
+		var x int
+		fmt.Println(sqlQuote(x))
+	}
+	section("7.14 token-based xml decoding")
+	{
+		// encoding/xml
+		// ch7/xmlselect
+		// ch1/fetch
+		var _ xml.Attr
+	}
+}
+
+func sqlQuote(x interface{}) string {
+	switch x := x.(type) {
+	case nil:
+		return "NULL"
+	case int, uint:
+		return fmt.Sprintf("%d", x) // x has type interface{} here.
+	case bool:
+		if x {
+			return "TRUE"
+		}
+		return "FALSE"
+	case string:
+		//return sqlQuoteString(x) // (not shown)
+		return x // (not shown)
+	default:
+		panic(fmt.Sprintf("unexpected type %T: %v", x, x))
 	}
 }
--- go/gopl.io/README.md
+++ go/gopl.io/README.md
@@ -19,7 +19,7 @@ book; you can safely ignore them.  In a few cases, programs
 have been reformatted in an unnatural way so that they can be presented
 in stages in the book.
 
-### 
+### environment
 ```shell
 ln -s $(pwd) $GOPATH/src/gopl.io
 ```
