113e4a7484c90bbe807411532243d9df6ac3b395
yubo <yubo@yubo.org>
2016-01-06 11:02:57 +0800

    add gopl.io/ch9

--- go/gopl.io/09_concurrency_with_shared_variables.go
+++ go/gopl.io/09_concurrency_with_shared_variables.go
@@ -15,9 +15,43 @@ func section(s string) {
 }
 
 func main() {
-	fmt.Println("# 9. Concurrency	with	Shared	Variables")
+	fmt.Println("# 9. Concurrency with Shared Variables")
 	section("9.1 Race Conditions")
 	{
-		// ch9/bank1
+		// ch9/bank1 // use chan for goroutine Communication
+	}
+	section("9.2 Mutual Exclusion: sync.Mutex")
+	{
+		// ch9/bank2 // use chan as semaphore to lock
+		// ch9/bank3 // use sync.Mutex to lock
+	}
+	section("9.3 Read/Write Mutexes: sync.RWMutex")
+	{
+		// sync.RWMutex
+	}
+	section("9.4 Memory Synchronization")
+	{
+		var x, y int
+		go func() {
+			x = 1                   // A1
+			fmt.Print("y:", y, " ") // A2
+		}()
+		go func() {
+			y = 1                   // B1
+			fmt.Print("x:", x, " ") // B2
+		}()
+	}
+	section("9.7 Example: Concurrent  Non-Blocking Cache")
+	{
+		// ch9/memo1  // lazyinit func() and cache
+		// ch9/memo2  // use sync.Mutex
+		// ch9/memo3
+		// ch9/memo4  // use <-close(chan struct{}) for check ready
+
+		// ch9/memo5 ****
+		// Broadcast the ready condition.
+		// close(e.ready)
+		// Wait for the ready condition.
+		// <-e.ready
 	}
 }
