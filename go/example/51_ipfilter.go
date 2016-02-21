1dcdc072c2eb84890ed099dd2605ad4a615cc649
yubo <yubo@yubo.org>
2016-02-21 20:17:04 +0800

    add popen

--- go/example/51_ipfilter.go
+++ go/example/51_ipfilter.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	ip := net.ParseIP("1.1.1.23")
-	_, net, _ := net.ParseCIDR("1.1.1.1/24")
+	_, net, _ := net.ParseCIDR("0.0.0.0/0")
 	fmt.Println(ip, net, net.Contains(ip))
 }
